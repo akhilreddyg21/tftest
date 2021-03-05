d0ff49c3a2ee2a3ddffe710fe09918c8857e7369
akhilreddyg <akhilreddyg@github.com>
2021-03-05 11:41:23 +0530

    adding managed disk tf code

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+
+variable "datadisk" {
+  type = object({
+    a = string
+    b = string
+    c = string
+  })
+  default = {
+    a            : "testdisk"
+    b            : "newdisk"
+    c            : "olddisk"
+  }
+}
+resource "azurerm_resource_group" "example" {
+  name     = "resourceGroup1"
+  location = "West US"
+}
+
+resource "azurerm_managed_disk" "name" {
+  for_each = var.datadisk
+
+  name = format("%s-%s", each.value, each.key)
+  location = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  storage_account_type = "StandardSSD_LRS"
+  create_option = "Empty"
+  disk_size_gb = "10"
+}
+
